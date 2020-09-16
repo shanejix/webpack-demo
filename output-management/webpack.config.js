943e3557891c28fa6784e20e77fb586f4bd26a92
shanejixx <shanejix@vip.163.com>
2020-09-16 23:02:24 +0800

    :rocket:feat(output): using clean-webpack-plugin

--- output-management/webpack.config.js
+++ output-management/webpack.config.js
@@ -7,6 +7,7 @@
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -22,6 +23,7 @@ module.exports = {
     rules: [],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Output Management",
     }),
