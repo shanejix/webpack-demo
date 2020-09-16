325fd4039fd0f499c2175b8abd05af57fd3dde3c
shanejixx <shanejix@vip.163.com>
2020-09-16 22:37:45 +0800

    :rocket:feat(asset): loading data

--- asset-management/webpack.config.js
+++ asset-management/webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ["file-loader"],
       },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"],
+      },
     ],
   },
   plugins: [],
