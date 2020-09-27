fbb508e0bc5e4dcc35ad197e19ddcd19b9812ff5
shanejixx <shanejix@vip.163.com>
2020-09-27 22:27:55 +0800

    :rocket:feat(libraries) :add stuff

---
+++ 11-author-libraries/webpack.config.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * https://webpack.js.org/guides/author-libraries/
+ *
+ */
+
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "webpack-numbers.js",
+    library: "webpackNumbers",
+    libraryTarget: 'umd'
+  },
+  externals: {
+    lodash: {
+      commonjs: "lodash",
+      commonjs2: "lodash",
+      amd: "lodash",
+      root: "_",
+    },
+  },
+};
